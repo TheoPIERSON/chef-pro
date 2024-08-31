5698e1893921a65794aab85f33fad73d1d7df89c
Theo Pierson <thheopierson@gmail.com>
2024-08-31 12:45:25 +0200

    feat: adapt style to big screens'

--- src/app/components/product-description.tsx
+++ src/app/components/product-description.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const ProductDescription = () => {
   return (
-    <ul>
+    <ul className="md:flex w-full justify-around mt-2">
       <li>
-        <h1 className="font-bold p-2 text-center text-xl text-slate-700">Most hergonomic handle ever made</h1>
-        <div className="flex items-center justify-center ">
-          <img src="assets/images/knife-handle.jpg" alt="" className="h-72 w-48 p-1" />
-          <p className="text-xs text-justify p-2">
+        <h1 className="font-bold p-2 text-center text-3xl text-slate-700 h-20 flex items-center justify-center">
+          Most hergonomic handle ever made
+        </h1>
+        <div className="flex md:grid items-center justify-center ">
+          <img src="assets/images/knife-handle.jpg" alt="" className="h-72 md:h-auto w-48 md:w-full p-1" />
+          <p className="text-xs md:text-lg text-justify p-2 h-64">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In pariatur consequuntur tenetur autem sapiente
             doloremque voluptatem excepturi, reprerit, eligendi aliquam dicta aliquid earum at ut et atque a laborum.
             Doloribus commodi nam accusamus officiis aliquid deserunt hic magnam. Accusantium ullam unde perferendis
@@ -17,26 +19,32 @@ const ProductDescription = () => {
         </div>
       </li>
       <li>
-        <h1 className="font-bold p-2 text-center text-xl text-slate-700">Long lasting sharp blade</h1>
-        <div className="flex items-center justify-center">
-          <p className="text-xs text-justify p-2">
-            Lorem ipsum dolor sit amet consectetur elit. In pariatur consequuntur tenetur autem sapiente doloremque
-            voluptatem excepturi, reprerit, eligendi aliquam dicta aliquid earum at ut et atque a laborum. officiis
-            aliquid deserunt hic magnam. ullam unde debitis optio iusto eius illo in, eveniet eum, illum laborum veniam
-            doloremque repellat alias ipsum molestiae quibusdam!
+        <h1 className="font-bold p-2 text-center text-3xl text-slate-700 h-20 flex items-center justify-center">
+          Long lasting sharp blade
+        </h1>
+        <div className="flex md:grid items-center justify-center">
+          <p className="text-xs md:text-lg text-justify p-2  h-64 mb-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In pariatur consequuntur tenetur autem sapiente
+            doloremque voluptatem excepturi, reprerit, eligendi aliquam dicta aliquid earum at ut et atque a laborum.
+            Doloribus commodi nam accusamus officiis aliquid deserunt hic magnam. Accusantium ullam unde perferendis
+            debitis optio eius illo in, eveniet eum, illum laborum veniam doloremque repellat alias ipsum molestiae
+            quibusdam!
           </p>
-          <img src="assets/images/knife-top.jpg" alt="" className="w-48 h-72 p-1" />
+          <img src="assets/images/knife-top.jpg" alt="" className="h-72 md:h-auto w-48 md:w-full p-1" />
         </div>
       </li>
       <li>
-        <h1 className="font-bold p-2 text-center text-xl text-slate-700">Made to cook anything</h1>
-        <div className="flex items-center justify-center">
-          <img src="assets/images/knife-salmon.jpg" alt="" className="w-48 h-72 p-1" />
-          <p className="text-xs text-justify p-2">
+        <h1 className="font-bold p-2 text-center text-3xl text-slate-700 h-20 flex items-center justify-center">
+          Made to cook anything
+        </h1>
+        <div className="flex md:grid  items-center justify-center">
+          <img src="assets/images/knife-salmon.jpg" alt="" className="h-72 md:h-auto w-48 md:w-full p-1" />
+          <p className="text-xs md:text-lg text-justify p-2  h-64">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In pariatur consequuntur tenetur autem sapiente
-            doloremque voluptatem dicta aliquid earum at ut et atque a laborum. Doloribus commodi nam accusamus officiis
-            aliquid deserunt hic magnam. ullam unde perferendis debitis optio iusto voluptatibus eius illo in, eveniet
-            eum, illum laborum veniam doloremque molestiae quibusdam!
+            doloremque voluptatem excepturi, reprerit, eligendi aliquam dicta aliquid earum at ut et atque a laborum.
+            Doloribus commodi nam accusamus officiis aliquid deserunt hic magnam. Accusantium ullam unde perferendis
+            debitis optio eius illo in, eveniet eum, illum laborum veniam doloremque repellat alias ipsum molestiae
+            quibusdam!
           </p>
         </div>
       </li>
