33b018496b02714c639f3446054e75a9fc57ac08
Theo Pierson <thheopierson@gmail.com>
2024-09-17 18:33:09 +0200

    feat: add picture for reviews

--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  return (
+    <div>
+      <div>
+        {" "}
+        <img src="assets/images/chef-1.jpg" />
+        <h2>Franck Chouquette</h2>
+        <p>The Chef Pro 360 really made a diffence in my cuisine, and my students love it too !</p>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
